108985f2f37f70edae47fc466102eb4918c04bf5
Amartya Aishwarya <amartya970914@gmail.com>
2018-12-26 16:30:42 +0530

    first commit-index_new_create

---
+++ app.js
@@ -0,0 +1,59 @@
+var bodyParser =  require("body-parser"), 
+    mongoose   = require("mongoose"),
+    express    = require("express"),
+    app        = express();
+
+//App-config
+mongoose.connect("mongodb://localhost/restful_blog_app",{ useNewUrlParser: true });
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+//Mongoose-model
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date,default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+//Restful Routes
+app.get("/", function(req, res){
+    res.redirect("/blogs");
+});
+
+
+//Index Route
+app.get("/blogs", function(req,res){
+   Blog.find({}, function(err,blogs){
+       if(err){
+           console.log("Error!");
+       }
+       else{
+           res.render("index", {blogs: blogs});
+       }
+   })
+    // res.render("index");
+});
+
+//new route
+app.get("/blogs/new", function(req,res){ 
+     res.render("new");
+});
+
+app.post("/blogs", function(req,res){
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        }
+        else{
+            res.redirect("/blogs");
+        }
+    });
+});
+
+app.listen(3000, function(){
+    console.log("Server Started");
+})
+
